bc94687046999cf6ddca18c8025ed40c747c6335
Denis <generalpokrichkin@icloud.com>
2024-04-11 23:48:53 +0300

    adding styles

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import './index.css';
+import BOOK_IMAGE from './assets/JS-01_image.jpeg';
 console.log('Hello world');
+
+const body = document.querySelector('body');
+
+const rendering = () => {
+	const pageTitle = document.createElement('h1');
+	pageTitle.textContent = 'I love JavaScript';
+	body.append(pageTitle);
+	const img = document.createElement('img');
+	img.src = BOOK_IMAGE;
+	body.append(img);
+	return { pageTitle, img };
+};
+rendering();
